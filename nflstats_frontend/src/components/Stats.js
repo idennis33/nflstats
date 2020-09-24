fd92515946c8820cf8a45783f9876ec3aaa30596
Isaac Dennis <idennisgsu3@gmail.com>
2020-09-24 13:07:07 -0400

    backend is rendering

--- nflstats_frontend/src/components/Stats.js
+++ nflstats_frontend/src/components/Stats.js
@@ -1,7 +1,18 @@
-import React,{useState,useEffect} from 'react'; 
+import React, { useState, useEffect } from "react";
 
-export default function Stats (){
-    return(
-        <h1>Stats</h1>
-    )
+export default function Stats(props) {
+  return (
+    <>
+      {props.stats.map((stat) => {
+        return (
+          <div key={stat.id} className="stat">
+            <h2>{stat.name}</h2>
+            <img src={stat.img} />
+            <h2>{stat.yards}</h2>
+            <h3>{stat.team}</h3>
+          </div>
+        );
+      })}
+    </>
+  );
 }
